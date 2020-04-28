d9484ef87f70edc7bf4d61ea102bc8a24db4b7e0
Kacomet <catfish5994@sbcglobal.net>
2020-04-28 05:40:34 -0500

    Can now be run at startup

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# /etc/init.d/main.py
+### BEGIN INIT INFO
+# Provides:          sample.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import time
 import threading
